c5739cf8ec9ca1bb7aab8a93244647bfb53fafe8
Sam Macbeth <sam@cliqz.com>
2018-06-08 13:39:32 +0200

    Remove unused code.

--- src/background.js
+++ src/background.js
@@ -1,29 +1,5 @@
 const { ConsentString } = require('consent-string');
 
-console.log('hello word', Spanan, ConsentString);
-const spanan = new Spanan.default();
-spanan.export({
-  getConsentStatus(tabId) {
-    console.log('called getConsentStatus');
-    return `got ${tabId}`;
-  }
-}, {
-  respond(response, request) {
-    browser.runtime.sendMessage({
-      response,
-      uuid: request.uuid
-    });
-  }
-});
-
-browser.runtime.onMessage.addListener((message) => {
-  spanan.handleMessage(message);
-});
-
-const runDetector = function(tab) {
-  browser.tabs.sendMessage(tab, { action: 'checkIAB' });
-}
-
 browser.runtime.onConnect.addListener((port) => {
   if (!port.sender.tab) {
     return;
