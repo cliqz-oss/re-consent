3820450cf82e637827d1f548813da5b440368111
Sam Macbeth <sam@cliqz.com>
2018-06-11 15:02:35 +0200

    Extract vendor specific consent cookie implementations.

--- src/iab-framework.js
+++ src/iab-framework.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { ConsentString } from 'consent-string';
 import createPageChannel from './page-actions';
 import moment from 'moment';
+import { EUConsentCookie, LocalStorageConsent, OilCookie } from './iab-vendors';
 
 export const PURPOSES = {
   1: 'Information storage and access',
@@ -9,53 +10,6 @@ export const PURPOSES = {
   3: 'Ad selection, delivery, reporting',
   4: 'Content selection, delivery, reporting',
   5: 'Measurement',
-}
-const localStorageKey = 'crfgL0cSt0r';
-
-function cookieWrapper(opts, url, thirdParty) {
-  if (browser.privacy.websites.firstPartyIsolate) {
-    opts.firstPartyDomain = thirdParty === true ? url.split('/')[2] : '';
-  }
-  return opts;
-}
-
-async function getConsentCookie(tab, consent) {
-  if (consent) {
-    // if consent is specified, look for a consent cookie matching the one returned from the CMP
-    const consentCookies = await browser.cookies.getAll(cookieWrapper({
-      name: 'euconsent',
-      storeId: tab.cookieStoreId,
-    }, tab.url, true));
-    const matchedCookie = consentCookies.find(c => c.value === consent.metadata);
-    if (matchedCookie) {
-      return matchedCookie;
-    }
-  }
-  const cookie = (await browser.cookies.getAll(cookieWrapper({
-    url: tab.url,
-    storeId: tab.cookieStoreId,
-  }, tab.url, false))).find(c => c.name.toLowerCase() === 'euconsent');
-  return cookie;
-}
-
-async function getConsentLSO(page, consent) {
-  const consentLSORaw = await page.getLocalStorage(localStorageKey);
-  if (consentLSORaw) {
-    return JSON.parse(consentLSORaw);
-  }
-  return null;
-}
-
-async function getOilCookie(tab) {
-  // oil.js implement does not store the consentString
-  // https://github.com/as-ideas/oil
-  const cookie = await browser.cookies.get(cookieWrapper({
-    // firstPartyDomain: host,
-    name: 'oil_data',
-    url: tab.url,
-    storeId: tab.cookieStoreId,
-  }, tab.url));
-  return cookie;
 }
 
 async function addVendorList(page, consent) {
@@ -66,62 +20,10 @@ async function addVendorList(page, consent) {
   consent.setGlobalVendorList(vendorList);
 }
 
-async function updateCookie(cookie, newValue) {
-  return browser.cookies.set({
-    expirationDate: cookie.expirationDate,
-    firstPartyDomain: cookie.firstPartyDomain || undefined,
-    domain: cookie.domain.startsWith('.') ? cookie.domain : undefined,
-    httpOnly: cookie.httpOnly,
-    name: cookie.name,
-    path: cookie.path,
-    secure: cookie.secure,
-    storeId: cookie.storeId,
-    url: `${cookie.httpOnly ? 'http' : 'https'}://${cookie.domain.startsWith('.') ? cookie.domain.substring(1) : cookie.domain}${cookie.path}`,
-    value: newValue,
-  });
-}
-
 export async function setConsentCookie(tab, consent, newConsent) {
   const page = createPageChannel(tab.id);
-  const [cookie, lso, oilCookie, ] = await Promise.all([
-    getConsentCookie(tab, consent),
-    getConsentLSO(page, consent),
-    getOilCookie(tab),
-    addVendorList(page, newConsent)
-  ]);
-  const consentUpdates = [];
-  if (cookie) {
-    console.log('update euconsent cookie', newConsent.getConsentString());
-    const setCookie = updateCookie(cookie, newConsent.getConsentString());
-    consentUpdates.push(setCookie);
-  }
-  if (lso) {
-    lso.consent = newConsent.getConsentString();
-    console.log(`update ${localStorageKey} localstorage value`, lso);
-    const setLSO = page.setLocalStorage(localStorageKey, JSON.stringify(lso));
-    consentUpdates.push(setLSO);
-  }
-  if (oilCookie) {
-    const oilData = JSON.parse(decodeURIComponent(oilCookie.value));
-    if (oilData.privacy === 1) {
-      oilData.privacy = {
-        1: false,
-        2: false,
-        3: false,
-        4: false,
-        5: false,
-      };
-    }
-    oilData.privacy = oilData.privacy || {};
-    Object.keys(oilData.privacy || {}).forEach((purpose) => {
-      oilData.privacy[purpose] = newConsent.allowedPurposeIds.indexOf(Number(purpose)) !== -1;
-    });
-    oilData.consentString = newConsent.getConsentString();
-    console.log('update oil.js cookie', oilData);
-    consentUpdates.push(updateCookie(oilCookie, JSON.stringify(oilData)));
-  }
-  await Promise.all(consentUpdates);
-  await page.resetCmp();
+  await addVendorList(page, newConsent);
+  await Promise.all(consent.storage.map((sto) => sto.update(newConsent)));
   consent.metadata = newConsent.getConsentString();
   return consent;
 }
@@ -134,14 +36,12 @@ export async function hasIabConsent(tab) {
     if (!consent) {
       return false;
     }
-    const [cookie, lso, oilCookie] = await Promise.all([
-      getConsentCookie(tab, consent),
-      getConsentLSO(page, consent),
-      getOilCookie(tab),
-    ]);
-    consent.hasConsentCookie = cookie ? true : false;
-    consent.hasConsentLSO = lso ? true : false;
-    consent.hasOilCookie = oilCookie ? true : false;
+    consent.storage = [
+      new EUConsentCookie(tab, consent),
+      new LocalStorageConsent(page),
+      new OilCookie(tab)
+    ];
+    consent.writeable = (await Promise.all(consent.storage.map((sto) => sto.exists()))).some((v) => v);
     return consent;
   } else {
     return false;
@@ -248,14 +148,13 @@ export class IABConsent extends Component {
   }
 
   render() {
-    const { metadata, purposeConsents, gdprApplies, hasConsentCookie, hasConsentLSO, hasOilCookie } = this.props.consent;
+    const { metadata, purposeConsents, gdprApplies, writeable } = this.props.consent;
     const consentData = new ConsentString(metadata);
-    const allowedPurposes = Object.keys(purposeConsents)
-      .filter(k => purposeConsents[k])
-      .reduce((l, v) => [...l, parseInt(v)], []);
-    const writeable = hasConsentCookie || hasConsentLSO || hasOilCookie;
     console.log('getVendorConsents', this.props.consent);
     console.log('consentCookieData', consentData);
+    const allowedPurposes = Object.keys(purposeConsents || {})
+      .filter(k => purposeConsents[k])
+      .reduce((l, v) => [...l, parseInt(v)], []);
     return (
       <div className="row">
         <div className="col-sm">
