0da3c40ea7ecb24274e60ed1c32b7aa096377d54
Panos Angelopoulos <panosangelopoulos@Panoss-MacBook-Pro.local>
2018-06-11 18:39:24 +0200

    Add button, badge and shadow components

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,18 +2,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Button from '../components/Button';
-
 import '../index.css';
 
 storiesOf('Button', module)
-  .add('default', () => (
-    <Button>
-      Show Settings
-    </Button>
-  ))
   .add('primary', () => (
-    <Button primary>
+    <button className="btn btn-primary">
       Deactivate
-    </Button>
+    </button>
+  ))
+  .add('secondary', () => (
+    <button className="btn btn-secondary">
+      Show Setting
+    </button>
+  ))
+  .add('link', () => (
+    <button className="btn btn-link">
+      What is this?
+    </button>
+  ))
+  .add('light', () => (
+    <div style={{ background: '#00AEF0', padding: '2em' }}>
+      <button className="btn btn-light">
+        Send a nightmare letter
+      </button>
+      &nbsp;
+      <button className="btn btn-outline-light">
+        What do they collect?
+      </button>
+    </div>
+  ));
+
+storiesOf('Badge', module)
+  .add('primary', () => (
+    <span className="badge badge-primary">
+      Deactivated
+    </span>
+  ))
+  .add('danger', () => (
+    <span className="badge badge-danger">
+      Active
+    </span>
+  ));
+
+storiesOf('Shadow', module)
+  .add('default', () => (
+    <div className="shadow p-2 m-2">
+      Lorem ipsum
+    </div>
   ));
