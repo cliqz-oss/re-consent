810b2196c532633f44a5b1956b6ae37b57eecfd4
Sebastian Slomski <sebastian@purelabs.io>
2018-08-08 15:11:36 +0200

    New styling for popup footer

--- src/components/popup/PopupFooter.jsx
+++ src/components/popup/PopupFooter.jsx
@@ -5,8 +5,10 @@ import { DETAIL_PAGE_URL } from './../../constants';
 
 const PopupFooter = () => (
   <div className="popup-footer">
-    <a className="btn btn-info" target="_blank" rel="noopener noreferrer" href={DETAIL_PAGE_URL}>
-      Show me the details
+    Learn more about
+    {' '}
+    <a className="link" target="_blank" rel="noopener noreferrer" href={DETAIL_PAGE_URL}>
+      Concentric &gt;
     </a>
   </div>
 );
