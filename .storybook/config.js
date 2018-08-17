6b18f0534167a6f47ddd24c4c62acdaaf9a5c090
Sebastian Slomski <sebastian@purelabs.io>
2018-08-17 10:38:57 +0200

    Updated dependencies & installed storybook react-intl integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,35 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
+import { addLocaleData } from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
+import deLocaleData from 'react-intl/locale-data/de';
+
+import translationsDe from '../src/translations/de.json';
+import translationsEn from '../src/translations/en.json';
+
+/* <ReactIntl Setup> */
+addLocaleData(enLocaleData);
+addLocaleData(deLocaleData);
+
+const translations = {
+  en: translationsEn,
+  de: translationsDe,
+};
+
+const getMessages = (locale) => {
+  return translations[locale];
+};
+
+setIntlConfig({
+  locales: ['en', 'de'],
+  defaultLocale: 'en',
+  getMessages,
+});
+
+addDecorator(withIntl);
+
+/* </ReactIntl Setup> */
+
 
 function loadStories() {
   require('../src/stories');
