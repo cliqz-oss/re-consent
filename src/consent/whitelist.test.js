b082a42c987881b002b7d9d1d0bbd4e7d10d3eff
Sebastian Slomski <sebastian@purelabs.io>
2018-08-21 11:52:32 +0200

    Whitelist via features

---
+++ src/consent/whitelist.test.js
@@ -0,0 +1,25 @@
+import checkIsWhiteListed from './whitelist';
+
+describe('consent', () => {
+  describe('whitelist', () => {
+    it('should whitelist consent checking for facebook', () => {
+      const url = new URL(`https://www.facebook.com`);
+      expect(checkIsWhiteListed(url)).toBeTruthy();
+    });
+
+    it('should whitelist consent checking for all google domains', () => {
+      const url = new URL(`https://www.google.it`);
+      expect(checkIsWhiteListed(url)).toBeTruthy();
+    });
+
+    it('should whitelist consent checking for twitter', () => {
+      const url = new URL(`https://www.twitter.com`);
+      expect(checkIsWhiteListed(url)).toBeTruthy();
+    });
+
+    it('should not whitelist chip.de', () => {
+      const url = new URL(`https://www.chip.de`);
+      expect(checkIsWhiteListed(url)).toBeFalsy();
+    });
+  });
+});
