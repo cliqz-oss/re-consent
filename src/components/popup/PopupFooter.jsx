89875e02a7e397456c0f7533f232aef0eab805a6
Sebastian Slomski <sebastian@purelabs.io>
2018-08-29 17:15:40 +0200

    Fix overflowing

--- src/components/popup/PopupFooter.jsx
+++ src/components/popup/PopupFooter.jsx
@@ -7,17 +7,19 @@ import { IconCliqzLogo } from '../Icons';
 
 const PopupFooter = () => (
   <div className="popup-footer">
-    <FormattedMessage
-      id="popup.footer.link"
-      values={{
-        consentricLink: (
-          <a className="link" target="_blank" rel="noopener noreferrer" href={DETAIL_PAGE_URL}>
-            Consentric &gt;
-          </a>
-        ),
-      }}
-    />
-    <div className="popup-footer__powered_by">
+    <div className="popup-footer__link-text">
+      <FormattedMessage
+        id="popup.footer.link"
+        values={{
+          consentricLink: (
+            <a className="link" target="_blank" rel="noopener noreferrer" href={DETAIL_PAGE_URL}>
+              Consentric &gt;
+            </a>
+          ),
+        }}
+      />
+    </div>
+    <div className="popup-footer__powered-by">
       Powered by <IconCliqzLogo />
     </div>
   </div>
