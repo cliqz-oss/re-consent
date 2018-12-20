fa48781e747e0eb4d9b0db2bdc2bf80eaa8311ea
Sam Macbeth <sammacbeth@users.noreply.github.com>
2018-12-20 11:12:51 +0100

    Prevent extension UUID leak via Origin on Firefox. (#8)

--- src/features/utils.js
+++ src/features/utils.js
@@ -1,5 +1,23 @@
+
+function fetchPolyfill(url) {
+  return new Promise((resolve) => {
+    const req = new XMLHttpRequest();
+    req.withCredentials = true;
+    req.addEventListener('load', () => {
+      console.log('loaded', url);
+      resolve({
+        status: req.status,
+        statusText: req.statusText,
+        text: () => Promise.resolve(req.responseText),
+      });
+    });
+    req.open('GET', url);
+    req.send();
+  });
+}
+
 export async function fetchDocument(url, format = 'html') { // eslint-disable-line import/prefer-default-export
-  const response = await fetch(url, { credentials: 'include' });
+  const response = await fetchPolyfill(url);
 
   if (response.status !== 200) {
     throw Error(response.statusText);
